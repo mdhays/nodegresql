1c44a109f722b1435eaab34d4dbf02c37800ce46
Matt Hays <mdhays1995@gmail.com>
2016-03-01 09:23:34 -0600

    Removed migrations.

--- index.js
+++ index.js
@@ -3,28 +3,33 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('postgres://localhost:5432/nodegresql');
 
-const Frienemy = sequelize.define('frienemy', {
+const Frienemy = sequelize.define('Frienemy', {
   name: Sequelize.STRING,
-  birthday: Sequelize.DATE
+  birthday: Sequelize.DATE,
+  friend: Sequelize.BOOLEAN
 });
-
-const Project = sequelize.define('project', {
+const Project = sequelize.define('projects', {
   name: Sequelize.STRING
 });
 
-Project.hasMany(Frienemy, {as: 'workers'});
+Project.hasMany(Frienemy);
+
+let jane;
 
 sequelize.sync().then(() => {
-  return Frienemy.create({
+  jane = Frienemy.create({
     name: 'Jane Doe',
     birthday: new Date('1980-3-4')
   });
 }).then(function(frienemy) {
-  console.log(frienemy.get({
-    plain: true
-  }));
+  console.log(jane);
 }).then(() => {
   return Project.create({
     name: 'Angular 101'
   });
+}).then((project) => {
+  // jane.addProject(project);
 });
+
+
+
